5226e8c6b881071ccf48c26149f0a663748b1e89
Tomas Suarez Paez <401747@sistemas.frc.utn.edu.ar>
2025-11-24 22:21:14 -0300

    agregaciones al securityConfig

--- TPI-Backend-G-/api-gateway/src/main/java/com/gestiontransporte/apigateway/config/SecurityConfig.java
+++ TPI-Backend-G-/api-gateway/src/main/java/com/gestiontransporte/apigateway/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
             // Deshabilitar CSRF (necesario para que Postman funcione sin tokens raros)
             .csrf(ServerHttpSecurity.CsrfSpec::disable)
             
-            // Configurar las reglas de autorización
+            // Configurar las reglas de autorizaciÃ³n
             .authorizeExchange(exchanges -> exchanges
 
                 // ATENTO ESTO LO ESCRIBIO UN SER HUMANO PENSANTE
@@ -25,18 +25,26 @@ public class SecurityConfig {
                 // grito para que prestes atencion
                 // el hasRole SIEMPRE en mayusculas mismo nombre que el keycloak pero en mayusculas
                  
+                //admin
                 .pathMatchers("/transporte/transportistas/**").hasRole("ADMIN")
                 .pathMatchers("/transporte/camiones/**").hasRole("ADMIN")
-                
+                .pathMatchers("/logistica/**").hasRole("ADMIN")
+                .pathMatchers("/solicitudes/**").hasRole("ADMIN")
                 .pathMatchers(HttpMethod.GET, "/precios/**").hasRole("ADMIN")
                 .pathMatchers(HttpMethod.PUT, "/precios/**").hasRole("ADMIN")
+                .pathMatchers(HttpMethod.GET, "/contenedores/**").hasRole("ADMIN")
+                
+                //transportista
+                .pathMatchers(HttpMethod.POST, "/api/logistica/tramos/**").hasAnyRole("TRANSPORTISTA", "ADMIN")
 
-
+                //cliente
                 .pathMatchers(HttpMethod.POST, "/solicitudes/**").hasRole("CLIENTE")
+                .pathMatchers(HttpMethod.GET, "/contenedores/**").hasRole("CLIENTE")
 
                 .pathMatchers("/api/logistica/**").hasRole("ADMIN")
+                
 
-                .anyExchange().authenticated()
+                .anyExchange().authenticated() 
             )
             .oauth2ResourceServer(oauth2 -> oauth2
                 .jwt(jwt -> jwt.jwtAuthenticationConverter(new KeycloakJwtAuthenticationConverter()))
